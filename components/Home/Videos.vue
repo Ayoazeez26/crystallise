2de806e9d0affabd07fef5ff5caf52683235d65c
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2023-08-29 19:44:36 +0100

    feat: renamed video

--- components/Home/Videos.vue
+++ components/Home/Videos.vue
@@ -5,8 +5,8 @@
     class="flex flex-col items-center mb-[100px] lg:mb-[200px] mx-auto px-4 xl:px-0 w-full max-w-[1240px]"
   >
     <video ref="video" controls loop class="mt-10">
-      <source src="/assets/Crystalize.mp4" type="video/mp4">
-      <p>Your browser does not support HTML video, here's a <a href="/assets/Crystalize.mp4">link </a> to watch the video</p>
+      <source src="/assets/crystalize.mp4" type="video/mp4">
+      <p>Your browser does not support HTML video, here's a <a href="/assets/crystalize.mp4">link </a> to watch the video</p>
     </video>
   </div>
 </template>
