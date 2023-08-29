97cea5485358fd1150ed52db9d172d3c4aefaccc
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2023-08-29 22:31:08 +0100

    feat: added video to s3

--- components/Home/Videos.vue
+++ components/Home/Videos.vue
@@ -5,8 +5,8 @@
     class="flex flex-col items-center mb-[100px] lg:mb-[200px] mx-auto px-4 xl:px-0 w-full max-w-[1240px]"
   >
     <video ref="video" controls loop class="mt-10">
-      <source src="/assets/crystalize.mp4" type="video/mp4">
-      <p>Your browser does not support HTML video, here's a <a href="/assets/crystalize.mp4">link </a> to watch the video</p>
+      <source src="https://s3.eu-west-2.amazonaws.com/crystallize.com/crystallize.mp4" type="video/mp4">
+      <p>Your browser does not support HTML video, here's a <a href="https://s3.eu-west-2.amazonaws.com/crystallize.com/crystallize.mp4">link </a> to watch the video</p>
     </video>
   </div>
 </template>
